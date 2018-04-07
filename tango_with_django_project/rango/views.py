64927cb07f3c59786b3b6ed24706b0a55f3ab720
alec_fielding <alecfielding95@gmail.com>
2018-04-06 17:26:59 -0700

    1.0.1
    commit

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 # Create your views here.
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
-    # Render the response and send it back! 
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request,category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug = category_name_slug)
+        pages = Page.objects.filter(category = category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html',context_dict)
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -6,4 +6,5 @@ from rango import views
 urlpatterns = [url(r'^$', views.index, name='index'),
                 url(r'about',views.about, name='about'),
                 url(r'index',views.index, name='index'),
+                url(r'category',views.show_category, name = 'category'),
                 url(r'^admin/', admin.site.urls)]
