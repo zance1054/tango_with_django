84a6e54b9c513ebf4db27cf3f6ae5a119b144bc6
Alec <32371145+zance1054@users.noreply.github.com>
2018-04-06 17:54:10 -0700

    Fixed
    code

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,8 +2,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page,PageAdmin)
-
-class PageAdmin extends admin.ModelAdmin:
+class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+    
+admin.site.register(Page,PageAdmin)
+admin.site.register(Category,CategoryAdmin)
